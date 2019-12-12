2ee9422751afdb4e960e6b5fdbe9cffc8943193f
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-12 00:34:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  case array.length
+    when 1
+      return array.join
+    when 2
+      return array.join(" and ")
+    else
+      return "#{array[0..-2].join(", ")}, and #{array.last}"
+    end
 
 end
